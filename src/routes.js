dbe376fff80d62a3aa3960e2764c91c594e984e5
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-07-25 15:31:38 -0300

    Github-Repository

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Repositorio from "./pages/Repositorio";
+
+function RouteApp() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Main /> } />
+      <Route path="/repositorio/:repositorio" element={ <Repositorio /> } />
+    </Routes>
+  );
+}
+
+export default RouteApp;
